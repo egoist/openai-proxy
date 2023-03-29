f89665ef974fe993ac262b5727c07413af9a51e7
EGOIST <hi@egoist.dev>
2023-03-29 23:12:34 +0800

    tweaks

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -4,24 +4,23 @@ export const config = {
   runtime: "edge", // this is a pre-requisite
   // exclude hongkong cause it's not supported by OpenAI
   regions: [
-    "arn1",
-    "bom1",
-    "bru1",
     "cdg1",
-    "cle1",
-    "cpt1",
+    "arn1",
     "dub1",
-    "fra1",
+    "lhr1",
+    "iad1",
+    "sfo1",
+    "pdx1",
+    "cle1",
     "gru1",
     "hnd1",
-    "iad1",
     "icn1",
     "kix1",
-    "lhr1",
-    "pdx1",
-    "sfo1",
     "sin1",
+    "bom1",
     "syd1",
+    "fra1",
+    "cpt1",
   ],
 };
 
